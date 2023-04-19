f1d8adfe3acb1d3f6f0c62d6a39ad5aa0ae18f8c
TPALT <tphualt@gmail.com>
2023-04-19 20:48:58 +0700

    Finalizing

--- php/set_new_password_action.php
+++ php/set_new_password_action.php
@@ -5,6 +5,7 @@
             $lmao = $_POST['pass'];
             mysqli_query($con, "UPDATE user_infos SET pass = '".$_POST['pass']."' WHERE username = '".$_SESSION['change-pass-username']."';");
             echo '<script>window.location.href = "../login.php";</script>';
+            unset($_SESSION['change-pass-username']);
         } else {
             echo '<script>document.getElementById("pass-check").innerHTML = "Hai mật khẩu không giống nhau";</script>';
         }
