da4aff4b907444ba340128a885865583a87a679b
TPALT <tphualt@gmail.com>
2023-05-08 20:46:07 +0700

    Xoá file thừa

--- php/index_action.php
+++
@@ -1,9 +0,0 @@
-<script src="./js/index.js"></script>
-
-<?php
-    $str = "none";
-    if (isset($_SESSION['username'])) {
-        $str = $_SESSION['username'];
-    }
-    echo "<script>init_index_buttons('".$str."');</script>";
-?>
--- test.php
+++ test.php
@@ -22,6 +22,5 @@ require_once("./html/header.php");
 <?php 
 echo file_get_contents("./html/footer.html");
 require_once("./php/connect_MySQL_n_log.php");
-require_once("./php/index_action.php");
 require_once("./php/test_get_data.php");
 ?>
