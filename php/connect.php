bca6d7b2b9dc91e01d83cb4ff325563ceae08c50
TPALT <tphualt@gmail.com>
2023-05-11 22:48:19 +0700

    NVM

--- php/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    $adr = "localhost";
-    $taikhoan = "root";
-    $matkhau = "";
-    $database = "webthi";
-    $con = mysqli_connect($adr, $taikhoan, $matkhau, $database) or die("Connect database fail!");
-?>
