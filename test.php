84451162ceb7ea9ae86250560b37e0054ce4cf9f
H4wkEyes <131275938+H4wkEye03@users.noreply.github.com>
2023-05-17 15:10:15 +0700

    ngu

--- test.php
+++ test.php
@@ -6,11 +6,25 @@ if (!isset($_SESSION['username'])) {
 require_once("./html/header.php");
 ?>
 
+<style>
+    input[type="button"] {
+  margin: 5px auto;
+  color: black;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  font-size: 16px;
+  cursor: pointer;
+  width: 150px; /* Đặt chiều rộng */
+  height: 40px; /* Đặt chiều cao */
+}
+</style>
+
 <h3 id="chapter-name">
 </h3>
 <div>
     <!-- Dev note: 2 cái input và div trong này chung 1 chỗ nhé -->
-    <input type="button" id="btn-start-timer" name="btn-start-timer" value="Bắt đầu làm bài!"></button>
+    <input type="button" id="btn-start-timer" name="btn-start-timer" value="Bắt đầu làm bài!">
     <div id="timer" name="timer"></div>
 </div>
 <div id="result"></div>
