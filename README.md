14b8d620766f56890396e65e046ffb949caa137f
TPALT <tphualt@gmail.com>
2023-05-31 22:55:35 +0700

    Delete Everything.

--- README.md
+++
@@ -1,2 +0,0 @@
-# ggcproject
-Quan trọng: Phải chạy file create_database_with_data đầu tiên để tạo database của web.
--- php/handle_closing_mock_exam.php
+++
@@ -1,8 +0,0 @@
-<?php 
-require_once("./connect_MySQL_n_log.php");
-session_start();
-$_SESSION['timeLeft'] = $_POST['timeLeft'];
-$test = $_POST['tableName'];
-echo $_POST['timeLeft'];
-unset($_POST['timeLeft'], $_POST['tableName']);
-?>
